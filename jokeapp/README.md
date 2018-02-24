1618644e288c1d18bdcb56b892c52948390a347e
jacobprajan <jjacobprajan@gmail.com>
2018-02-24 16:56:01 +0000

    SpringFrameWork5-commit12-Building A Spring Boot Jokes App

---
+++ jokeapp/README.md
@@ -0,0 +1,3 @@
+# Spring Boot Jokes App
+This Spring Boot Web Application displays a random Chuck NOrris Joke
+on the index
---
+++ jokeapp/src/main/java/com/dev/services/JokeService.java
@@ -0,0 +1,9 @@
+package com.dev.services;
+
+/**
+ * Created by Jakez on 24/02/2018.
+ */
+public interface JokeService {
+
+    String getJoke();
+}
