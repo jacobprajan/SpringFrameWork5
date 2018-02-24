d5b01c6d162881952fafb80f166634b063dd5e8f
jacobprajan <jjacobprajan@gmail.com>
2018-02-24 01:38:45 +0000

    SpringFrameWork5-commit10-Spring Assignment

---
+++ spring5-di-demo-di-assignment/src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -0,0 +1,30 @@
+package guru.springframework.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jt on 5/24/17.
+ */
+@Service
+@Primary
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
+
+    // Two ways of fixing
+    // 1. Add @Autowired annotation to GreetingRepository
+    // 2. Add a constructor with dependency GreetingRepository. Spring will do autowiring
+
+    public PrimaryGermanGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
+    private GreetingRepository greetingRepository;
+
+    @Override
+    public String sayGreeting() {
+        return greetingRepository.getGermanGreeting();
+    }
+}
