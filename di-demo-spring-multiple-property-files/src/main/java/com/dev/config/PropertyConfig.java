009d2b02a70c21e6fd328460a8d812c6726abed2
jacobprajan <jjacobprajan@gmail.com>
2018-02-26 15:09:24 +0000

    Commit19-SpringFrameWork5-Spring-Multiple-Property-Files

---
+++ di-demo-spring-multiple-property-files/src/main/java/com/dev/config/PropertyConfig.java
@@ -0,0 +1,73 @@
+package com.dev.config;
+
+import com.dev.examplebeans.FakeDataSource;
+import com.dev.examplebeans.FakeJmsBroker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+
+/**
+ * Created by Jakez on 26/02/2018.
+ */
+
+@Configuration
+
+// Spring 4
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties" })
+
+// Spring 5
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+})
+
+public class PropertyConfig {
+
+    @Autowired
+    Environment env;
+
+    @Value("${dev.username}")
+    String user;
+    @Value("${dev.password}")
+    String password;
+    @Value("${dev.dburl}")
+    String url;
+
+    @Value("${dev.jms.username}")
+    String jmsUsername;
+    @Value("${dev.jms.password}")
+    String jmsPassword;
+    @Value("${dev.jms.url}")
+    String jmsUrl;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker() {
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUser(jmsUsername);
+        fakeJmsBroker.setPassword(jmsPassword);
+        fakeJmsBroker.setUrl(jmsUrl);
+        return fakeJmsBroker;
+    }
+
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
---
+++ di-demo-spring-multiple-property-files/src/main/java/com/dev/services/GreetingService.java
@@ -0,0 +1,10 @@
+package com.dev.services;
+
+/**
+ * Created by Jakez on 23/02/2018.
+ */
+public interface GreetingService {
+
+    public String sayGreeting();
+
+}
